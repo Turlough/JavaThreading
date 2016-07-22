0b6afa4928baef41ef8d68952755be63b40ee9fd
turlough <turloughcowman@gmail.com>
2016-07-22 16:33:28 +0100

    commited an empty test

---
+++ src/test/java/com/example/turlough/menu/MenuRunnerTest.java
@@ -0,0 +1,19 @@
+package com.example.turlough.menu;
+
+import org.junit.Test;
+
+/**
+ * Created by turlough on 22/07/16.
+ */
+public class MenuRunnerTest {
+    @Test
+    public void factorial() throws Exception {
+
+    }
+
+    @Test
+    public void sendSocketMessage() throws Exception {
+
+    }
+
+}
